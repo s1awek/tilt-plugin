9ca29abaa1cd2d54f79ab6f69082dc7cc7061150
Slawek Kasprzak <slawomir.kasprzak@oxmedia.pl>
2018-02-08 10:48:53 +0100

    Added forgotten files

---
+++ wp-content/themes/twentyfifteen-child/index.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+<script src="http://www.thepetedesign.com/demos/tiltedpage_scroll/jquery.tiltedpage-scroll.js"></script>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php
+			$counter = 0;
+			// Start the loop.
+			while ( have_posts() ) : the_post();
+			$counter++;
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				?>
+				<section class="page<?php echo $counter?>"> <?php
+				get_template_part( 'content', get_post_format() );
+				?>
+				</section>
+				<?php
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+	<script>
+	
+(function($){
+  
+	$(".main").tiltedpage_scroll({
+    sectionContainer: "> section",     // In case you don't want to use <section> tag, you can define your won CSS selector here
+    angle: 50,                         // You can define the angle of the tilted section here. Change this to false if you want to disable the tilted effect. The default value is 50 degrees.
+    opacity: true,                     // You can toggle the opacity effect with this option. The default value is true
+    scale: true,                       // You can toggle the scaling effect here as well. The default value is true.
+    outAnimation: true                 // In case you do not want the out animation, you can toggle this to false. The defaul value is true.
+  });
+})(jQuery);
+	</script>
+
+<?php get_footer(); ?>
